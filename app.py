27e4e93231f616c0e907c87fc13206658e5093dc
savanismit <savanismit11@example.com>
2020-04-17 10:34:33 +0530

    Hello World!

--- app.py
+++ app.py
@@ -6,7 +6,14 @@ app = Flask(__name__)
 
 @app.route('/',methods=['GET','POST'])
 def test():
-	return ("Hello!")
+    num = request.form.get("From")
+    num = num.replace("whatsapp:","")
+    msg = request.form.get("Body")
+
+    resp = MessagingResponse()
+    msg = resp.message("Hey,Good Morning!") 
+    
+    return(str(resp))
 
 if __name__ == '__main__':
 	app.run(debug=True)
